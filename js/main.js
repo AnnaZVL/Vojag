6d33e779704744c0bdcad6b2f4cc25ec634ef445
Anna Zadiraka <annazadiraka@gmail.com>
2024-05-21 15:03:28 +0300

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -88,4 +88,127 @@ const swiper1 = new Swiper('.swiper_1', {
     theme: 'blue',
   });
 
+// Вызов меню и бургер-кнопка
+const $burger = document.querySelector('.burger'),
+$headerMenu = document.querySelector('.header-bottom');
+
+$burger.addEventListener('click', () => {
+  $burger.classList.toggle('open');
+  $headerMenu.classList.toggle('open');
+});
+
+//=======================================================================
+
+//Поиск
+const $btnSearch = document.querySelector('.btn__serch'),
+    $btnSearchActive = document.querySelector('.btn__serch-active'),
+    $formSearch = document.querySelector('.menu__serch');
+
+$formSearch.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+$btnSearchActive.addEventListener('click', () => {  
+  $formSearch.classList.add('active');
+});
+
+$btnSearch.addEventListener('click', () => {  
+  $formSearch.classList.remove('active');
+});
+
+//=======================================================================
+//Кнопка "В заказ"
+const $btnCard= document.querySelectorAll('.card__btn'),
+    $cardText = document.createElement('button'),
+    $btnOrder = document.querySelector('.card__btn-order'),
+    $btnPlus = document.createElement('button'),
+    $btnMinus =  document.createElement('button');    
+const $btnAdd = document.createElement('div'),      
+    $cardBtnValue = document.createElement('span'),
+    $cardBtnText = document.createElement('span');
+
+    $btnCard.forEach(elem => {
+      elem.addEventListener('click', () => {        
+        setTimeout( () => {
+          elem.append(createdBtnAdd())
+        }, 300);
+        elem.classList.add('active');
+        //document.querySelector('.card__btn-order').remove();
+      });
+      
+      
+    });
+
+
+//Создание кнопки "Добавления набора"
+function createdBtnAdd (prop = '1') {
+
+
+    $btnPlus.classList.add('card__btn-plus','text', 'btn-resert');
+    $btnPlus.textContent = '+';
+    $btnMinus.classList.add('card__btn-minus','text', 'btn-resert');
+    $btnMinus.textContent = '-';
+
+    $cardBtnValue.classList.add('card__btn-value');
+    $cardBtnValue.textContent = prop;
+
+    switch (prop) {
+      case '1': 
+        $cardBtnText.textContent = 'набор';
+       break
+      case '2' || '3' || '4': 
+        $cardBtnText.textContent = 'набора';
+        break
+      default:
+        $cardBtnText.textContent = 'наборов';      
+    };    
+
+    $cardText.classList.add('card__btn-text', 'text', 'btn-resert');
+    $cardText.append($cardBtnValue, $cardBtnText);
+
+    $btnAdd.append($btnMinus, $cardText, $btnPlus);
+    $btnAdd.classList.add('card__btn-add');    
+   
+
+  return $btnAdd
+}
+//=======================================================================
+//Добавление/удаление позиции в корзину
+// const $btnMinus= document.querySelector('.card__btn-minus'),
+//     $btnPlus = document.querySelector('.card__btn-plus'),
+    const $order = document.querySelector('.order'),
+    $orderAll = document.querySelector('.price'),
+    $price = document.querySelector('.price-number');
+
+// $btnMinus.addEventListener('click', () => {
   
+//   $orderAll.textContent = Number($orderAll.textContent) - Number($price.textContent);
+//   if (Number($orderAll.textContent) == 0) $order.classList.remove('active');
+// })
+// let n = 1;
+// $btnPlus.addEventListener('click', () => {
+//   n += 1;
+//   createdBtnAdd(n)
+//   $order.classList.add('active');
+//  $orderAll.textContent = Number($orderAll.textContent) + Number($price.textContent)
+// })
+
+
+$cardText.addEventListener('click', () => {
+  $order.classList.add('active');
+  $orderAll.textContent = Number($orderAll.textContent) + Number($price.textContent)
+})
+
+  
+/*Вызов конзины*/
+
+const $basket = document.querySelector('.basket');
+
+$order.addEventListener('click', () => {
+  $basket.classList.add('open')
+})
+
+// document.body.addEventListener('click', (e) => {
+//   if ($basket.classList.contains('.open')) {console.log('object');}
+//     //if (e.target.matches('.basket')) 
+// })
